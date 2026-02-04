abbe0c5aff6083b7c1ccaa024d2a54224eb97d8a
emeyer25 <emeyer2025@gmail.com>
2026-02-03 19:02:44 -0500

    Initial Commit

---
+++ DemoApp/DemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppApp.swift
+//  DemoApp
+//
+//  Created by Eric Meyer on 2/3/26.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
